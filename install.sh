bdc841d198de7a2db6ba3b17783c722e9353d315
Sam <samgauck@icloud.com>
2020-10-01 17:25:42 -0400

    use the same style for install and uninstall

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 echo "Making all scripts executable..."
 chmod +x helpme*
-echo "   done"
+echo "...done"
 
 echo
 
@@ -11,14 +11,14 @@ mv helpme-delete.py /usr/local/bin/helpme-delete
 mv helpme-create.py /usr/local/bin/helpme-create
 mv helpme-completions.py /usr/local/bin/helpme-completions
 mv helpme-list.sh /usr/local/bin/helpme-list
-echo "   done"
+echo "...done"
 
 echo
 
 echo "Moving completion script into ~/.bash_completion.d ..."
 mkdir ~/.bash_completion.d
 mv helpme_complete.sh ~/.bash_completion.d/
-echo "   done"
+echo "...done"
 
 echo
 
