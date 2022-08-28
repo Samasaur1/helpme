6b22f41a5a7cc337484b7bc507bbee06a823ff91
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-08-28 12:18:52 -0700

    Copy instead of moving files

--- install.sh
+++ install.sh
@@ -5,19 +5,19 @@ echo "...done"
 echo
 
 echo "Moving scripts to /usr/local/bin/ ..."
-mv helpme.py /usr/local/bin/helpme
-mv helpme-edit.py /usr/local/bin/helpme-edit
-mv helpme-delete.py /usr/local/bin/helpme-delete
-mv helpme-create.py /usr/local/bin/helpme-create
-mv helpme-completions.py /usr/local/bin/helpme-completions
-mv helpme-list.sh /usr/local/bin/helpme-list
+cp helpme.py /usr/local/bin/helpme
+cp helpme-edit.py /usr/local/bin/helpme-edit
+cp helpme-delete.py /usr/local/bin/helpme-delete
+cp helpme-create.py /usr/local/bin/helpme-create
+cp helpme-completions.py /usr/local/bin/helpme-completions
+cp helpme-list.sh /usr/local/bin/helpme-list
 echo "...done"
 
 echo
 
 echo "Moving completion script into ~/.bash_completion.d ..."
 mkdir ~/.bash_completion.d
-mv helpme_complete.sh ~/.bash_completion.d/
+cp helpme_complete.sh ~/.bash_completion.d/
 echo "...done"
 
 echo
